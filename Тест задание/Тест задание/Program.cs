e90a89a80320e2e09a59c983a5841950aecba277
E-noTi-kgit config --global user.name E-noTi-kgit config --global user.name E-noTi-k <labovichaleks@gmail.com>
2024-10-19 11:32:42 +0300

    Сделали сложение переменных

--- Тест задание/Тест задание/Program.cs
+++ Тест задание/Тест задание/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 int a = 1;
 int b = 2;
+int sum = a + b;
+Console.WriteLine(sum);
